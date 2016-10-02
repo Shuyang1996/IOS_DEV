814f66d3925399842ed4cfa03f7a6bc5f65b6bc0
Shuyang Yu <yu2@cooper.edu>
2016-10-02 16:28:16 -0400

    attach view controller files to view controllers on the story board

---
+++ Compare/Compare/SocialViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SocialViewController.swift
+//  Compare
+//
+//  Created by 于舒洋 on 10/2/16.
+//  Copyright © 2016 WB. All rights reserved.
+//
+
+import UIKit
+
+class SocialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
