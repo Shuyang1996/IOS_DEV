1322cac74e48ea2f6d97c51afeda72a5e7d2c0a2
Shuyang Yu <yu2@cooper.edu>
2016-10-04 14:54:43 -0400

    segment controlled table ready

--- Compare/Compare/SocialViewController.swift
+++ Compare/Compare/SocialViewController.swift
@@ -8,45 +8,84 @@
 
 import UIKit
 
-class SocialViewController: UIViewController{
+class SocialViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     //properties
-    @IBOutlet weak var socialLabel: UILabel!
+    
     @IBOutlet weak var typeSelector: UISegmentedControl!
-   
+    @IBOutlet weak var profilePicTable: UITableView!
+    
+    
+    //hard coded data for now
+    let friends:[String] = ["Friend1","Friend2","Friend3","Friend4","Friend5","Friend6"]
+    let me:[String] = ["My pic 1          20 votes",
+                       "My pic 2          10 votes",
+                       "My pic 3          5   votes",
+                       "My pic 4          1   votes",
+                       "My pic 5          2   votes",
+                       "My pic 6          9   votes",
+                       "My pic 7          8   votes",
+                       "My pic 8          16 votes"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     
     
-    
-    //action
-    @IBAction func pageSwtich(sender: UISegmentedControl) {
-        if( typeSelector.selectedSegmentIndex == 0){
-            socialLabel.text = "Friends"
-        }else{
-            socialLabel.text = "Me"
+    // required functions for UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var returnValue = 0
+        
+        switch(typeSelector.selectedSegmentIndex)
+        {
+        case 0:
+            returnValue = friends.count
+            break
+        case 1:
+            returnValue = me.count
+            break
+            
+        default:
+            break
         }
+            
+        return returnValue
+    }
+        
+   
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let myCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
+        
+        switch(typeSelector.selectedSegmentIndex) {
+            case 0:
+                myCell.textLabel!.text = friends[indexPath.row]
+                break
+            case 1:
+                myCell.textLabel!.text = me[indexPath.row]
+                break
+            
+            default:
+                break
+            
+        }
+        
+        return myCell
         
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //Action:
+    @IBAction func tableSwitch(sender: UISegmentedControl) {
+        profilePicTable.reloadData()
     }
-    */
-
+    
+    
 }
