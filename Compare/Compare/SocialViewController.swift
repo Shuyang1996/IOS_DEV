cc23f2ffb00bbd6b3586a028f54e21b6d46d22a8
Shuyang Yu <yu2@cooper.edu>
2016-10-05 01:34:37 -0400

    set up image picker and update profile page photo cells “

--- Compare/Compare/SocialViewController.swift
+++ Compare/Compare/SocialViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class SocialViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SocialViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //properties
     
     @IBOutlet weak var typeSelector: UISegmentedControl!
     @IBOutlet weak var profilePicTable: UITableView!
+    @IBOutlet weak var currentProfilePic: UIImageView!
     
     
     //hard coded data for now
@@ -82,10 +83,46 @@ class SocialViewController: UIViewController, UITableViewDataSource, UITableView
         
     }
     
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        currentProfilePic.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
+    
+    
     //Action:
+    
+    //#1 load table data
     @IBAction func tableSwitch(sender: UISegmentedControl) {
         profilePicTable.reloadData()
     }
+    
+    //#2 set up image picker
+    @IBAction func imagePicker(sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+   
+    
     
     
 }
